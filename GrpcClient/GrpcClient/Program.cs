b79f1364c4cfb172e33ee1b9508752d20dc477ef
Mountain Cat <tomasz.banas123@gmail.com>
2024-03-28 11:54:55 +0100

    init

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,7 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5275");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+Console.WriteLine("Greeting: " + reply.Message);
